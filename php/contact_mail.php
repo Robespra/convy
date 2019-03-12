caa7c9866a93cd24e88566499699c0b43f22a006
Robespra <rpages07@gmail.com>
2019-03-12 14:53:37 +0100

    cleaned up

--- php/contact_mail.php
+++
@@ -1,9 +0,0 @@
-<?php
-$toEmail = "hello@convy.io";
-$mailHeaders = "From: " . $_POST["userName"] . "<". $_POST["userEmail"] .">\r\n";
-if(mail($toEmail, $_POST["userName"], $mailHeaders)) {
-print "<p class='success'>Your request has been correctly sent. We will provide feedback within 48h. Thank you.</p>";
-} else {
-print "<p class='Error'>Problem in sending form, please try again later</p>";
-}
-?>
